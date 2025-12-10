571fe9ce51127e142383dd6dbca6e186bd334928
Ronnie Low <ronnielow@protonmail.com>
2025-12-11 06:27:00 +1100

    day2p1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
-use std::error::{Error};
-use regex::{Regex};
+use ilog::IntLog;
+use std::error::Error;
+use std::num::ParseIntError;
+use regex::Regex;
 
 struct ParseType {
     ubound: u64,
@@ -8,37 +10,104 @@ struct ParseType {
 }
 
 impl ParseType {
-    fn new() -> Self {
+    fn new(lb: u64, ub: u64) -> Self {
         ParseType {
-            ubound: 0,
-            lbound: 0,
+            lbound: lb,
+            ubound: ub,
         }
     }
 }
+
+impl Iterator for ParseType {
+    type Item = u64;
+
+    fn next(self: &mut Self) -> Option<u64> {
+        if self.lbound <= self.ubound {
+            self.lbound += 1;
+            Some(self.lbound)
+        } else {
+            None
+        }
+    }
+}
+
+trait U64EXT {
+    fn digits(&self) -> u64;
+    fn index_slice(&self, n: u64) -> Option<Vec<u64>>;
+}
+
+impl U64EXT for u64 {
+    fn digits(&self) -> u64 {
+        self.log10() as u64 + 1
+    }
+
+    fn index_slice(&self, n: u64) -> Option<Vec<u64>> {
+        let digits = self.digits();
+        let str: String = self.to_string();
+        let mut strvec: Vec<&str> = Vec::new();
+
+        if digits % n != 0 { return None; }
+        for i in 0..n {
+            let start: usize = (i * digits / n) as usize;
+            let end: usize = ((i + 1) * digits / n) as usize;
+            strvec.push(&str[start..end]);
+        }
+
+        let res = strvec.iter().map(|x| x.parse::<u64>()).collect();
+        match res {
+            Ok(n) => Some(n),
+            Err(..) => panic!("What the shrigma"),
+        }
+    }
+}
+
 
 fn main() {
     let mut id_in = String::new();
     io::stdin().read_line(&mut id_in).unwrap();
 
-    solve(&id_in);
-    println!("exit");
+    let soln = solve(&id_in);
+    println!("soln: {}", soln);
 }
 
-fn solve(s: &String) -> Box<dyn Error> {
+// finds repetitions of numbers within int
+fn rept_n(pt: &mut ParseType) -> u64 {
+    let mut rept_sum = 0;
+
+    'has_match: for i in pt {
+        let slices: Option<Vec<u64>> = i.index_slice(2);
+        let mut svec: Vec<u64> = match slices {
+            Some(n) => n,
+            None => continue,
+        };
+
+        let base = match svec.pop() {
+            Some(n) => n,
+            None => continue,
+        };
+        for item in svec {
+            if base != item {
+                continue 'has_match;
+            }
+        }
+
+        rept_sum += i;
+    }
+
+    rept_sum
+}
+
+fn solve(s: &String) -> u64 {
     let mut i = 0;
+    let mut sum: u64 = 0;
     loop {
         let pres: Result<ParseType, Box<dyn Error>> = parse(s, i);
-        let pt: ParseType = match pres {
-            Ok(n) => {
-                n 
-            }
-            Err(e) => {
-                println!("End reached, error: {}", e);
-                return e;
-            }
+        let mut pt: ParseType = match pres {
+            Ok(n) => n,
+            Err(..) => return sum,
         };
-        println!("{}", pt.lbound < pt.ubound);
 
+        sum += rept_n(&mut pt);
         i += 1
     }
 }
@@ -48,13 +117,9 @@ fn solve(s: &String) -> Box<dyn Error> {
 // i.e. 11-22,95-115,998-1012,1188511880-1188511890,222220-222224 for index 2 would return
 // lower bound of 998 and upper bound of 1012
 fn parse(s: &String, i: i32) -> Result<ParseType, Box<dyn Error>> {
-    let range: Regex = Regex::new(",")?;
-    let low_high: Regex = Regex::new("-")?;
-    let res: Vec<u64> = low_high.split(range.split(s).collect::<Vec<&str>>()[i as usize])
-        .map(|x| x.parse::<u64>()).collect::<Result<_, _>>()?;
-
-    Ok(ParseType {
-        lbound: res[0],
-        ubound: res[1],
-    })
+    Regex::new("-")?
+        .split(Regex::new(",|\n|\0")?.split(s).collect::<Vec<&str>>()[i as usize])
+        .map(|x| x.parse::<u64>())
+        .collect::<Result<Vec<u64>, ParseIntError>>()
+        .map(|v| Ok(ParseType::new(v[0], v[1])))?
 }
